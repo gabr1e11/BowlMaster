7f4aa549ed2fe6e3814248d48f9904f229c079ab
Roberto Cano <roberto.cano@gmail.com>
2017-04-25 13:01:52 +0200

    first commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float LaunchSpeed;
+
+    Rigidbody m_rigidBody;
+    AudioSource m_audioSource;
+
+    void Awake()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+        m_audioSource = GetComponent<AudioSource>();
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        Launch();
+    }
+
+    public void Launch()
+    {
+        m_rigidBody.velocity = Vector3.forward * LaunchSpeed;
+        m_audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public Ball ball;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - ball.transform.position;
+        transform.position = ball.transform.position + offset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ball.transform.position.z < 1800.0f)
+        {
+            transform.position = ball.transform.position + offset;
+        }
+    }
+}
